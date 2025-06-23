24d37938c8ba7ab188b263172e75289c1e27213c
Thuytien4325 <thuyytien4325@gmail.com>
2025-06-23 15:57:17 +0700

    Update ServiceType.java

--- src/main/java/com/dna_testing_system/dev/enums/ServiceType.java
+++ src/main/java/com/dna_testing_system/dev/enums/ServiceType.java
@@ -1,8 +1,20 @@
-package com.dna_testing_system.dev.enums;
+package com.dna_testing_system.dev.enum;
 
 public enum ServiceType {
-    PATERNITY_TEST,
-    MATERNITY_TEST,
-    GENETIC_SCREENING,
-    OTHER
+    CIVIL("Civil Service"),
+    ADMINISTRATIVE("Administrative Service"),
+    PATERINITY_TEST("Paternity Test"),
+    MATERNITY_TEST("Maternity Test"),
+    GENETIC_SCREENING("Genetic Screening"),
+    OTHER("Other");
+
+    private final String description;
+
+    ServiceType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
